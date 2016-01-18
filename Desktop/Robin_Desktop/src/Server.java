b5ec25b79274be99295ca02a453a8e04a0e4ed31
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-01-17 19:48:48 -0500

    App now can send post requests to server, which can accept them and output them

--- Desktop/Robin_Desktop/src/Server.java
+++ Desktop/Robin_Desktop/src/Server.java
@@ -1,10 +1,30 @@
+import socketio.*;
 
 public class Server {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
-		System.out.println("sup");
+
+        Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(9092);
+
+        final SocketIOServer server = new SocketIOServer(config);
+        server.addEventListener("chatevent", ChatObject.class, new DataListener<ChatObject>() {
+            @Override
+            public void onData(SocketIOClient client, ChatObject data, AckRequest ackRequest) {
+                // broadcast messages to all clients
+                server.getBroadcastOperations().sendEvent("chatevent", data);
+            }
+        });
+
+        server.start();
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+        server.stop();
+		
 	}
 
 }
--- RobinApp/app/src/main/java/bd/com/robinapp/MainActivity.java
+++ RobinApp/app/src/main/java/bd/com/robinapp/MainActivity.java
@@ -1,13 +1,77 @@
 package bd.com.robinapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            SendDataTask task = new SendDataTask();
+            task.execute();
+
+        }
+
     }
-}
+
+    class SendDataTask extends AsyncTask<Void, Void, Void> {
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+                URL url = new URL("http://7d58ba52.ngrok.io");
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                // output an ack
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("User-Agent", "ROBIN");
+                connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+                connection.setRequestProperty("Content-Type", "text/html");
+
+                PrintWriter out = new PrintWriter(connection.getOutputStream());
+
+                out.println("do you hear me?");
+                out.println("break"); // to tell it to stop reading here
+
+                out.flush();
+
+                Log.d("Response", connection.getResponseMessage());
+
+//                BufferedReader in = new BufferedReader(connection.getInputStream());
+//
+//                String inputLine = " ";
+//
+//                // get the response
+//                while (!in.readLine().equals(null))
+//                    inputLine += in.readLine();
+//
+//                Log.d("More Response", inputLine);
+
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+
+
+
